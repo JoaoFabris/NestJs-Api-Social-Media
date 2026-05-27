49eba5c910cc03d0e144b41369ba9987857ba789
fabris <jvfabris21@gmail.com>
2026-05-27 11:53:53 -0300

    chore: remove unused bcrypt and LocalAuthGuard imports

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -20,7 +20,6 @@ import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
-import { LocalAuthGuard } from "src/auth/guards/local-auth.guard";
 import { FileInterceptor } from "@nestjs/platform-express";
 
 @Controller("users")
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,7 +5,6 @@ import {
 } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
-import * as bcrypt from "bcrypt";
 
 import { User } from "./entities/user.entity";
 import { CreateUserDto } from "./dto/create-user.dto";
